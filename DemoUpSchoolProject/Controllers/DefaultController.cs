7d1d1c2f9f734808c1e0c50db9a97252e2b8f113
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2022-10-14 21:22:15 +0300

    Add project files.

---
+++ DemoUpSchoolProject/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
+        public IActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
